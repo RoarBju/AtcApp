243ec5bf4c3bb5e20f3d890f976bf8a0069c4ca5
RoarBju <roar.bjurstrom@gmail.com>
2014-02-28 18:47:36 +0100

    randompush

---
+++ www/js/build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: ".",
+    paths: {
+        jquery: 'lib/jquery-1.10.2.min',
+        underscore: 'lib/underscore-min',
+        backbone: 'lib/backbone-min'
+    },
+    name: "main",
+    out: "main-built.js"
+})
